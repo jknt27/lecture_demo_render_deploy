73f2322de5e8270140bdb73e328cfcb6dad42b06
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-10-10 09:38:50 -0700

    adding endpoint hello

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//     res.send('Hello');
-// });
+app.get('/hello', (req, res) => {
+    const name = req.query.name;
+    res.type('html')
+    res.send(`
+        <html>
+            <head>
+                <title>Hello ${name}</title>
+            </head>
+            <body>
+                <h1>Hello ${name}!</h1>
+            </body>
+        </html>
+    `);
+});
 
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
