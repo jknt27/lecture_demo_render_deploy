5da7a692971e3f800991704092a1f7e2d2c0b063
Joseph Tran <100446142+jknt27@users.noreply.github.com>
2023-10-10 09:31:10 -0700

    Simple working server to test render deployment

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello');
+// });
+
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`);
+});
